d0ba90a9a13361704fbe30eb50ebbe53a59eaa4c
DilshanRajika9835 <dilshanrajitha1998@gmail.com>
2021-10-02 20:19:26 +0530

    Change license

--- src/lk/ijse/A.java
+++ src/lk/ijse/A.java
@@ -1,6 +1,6 @@
 /*
  * ---------------------------------------------------------------------------------------------
- *   Copyright (c) $today.year.DR Corporation. All rights reserved.
+ *   Copyright (c) 2021 DR Corporation. All rights reserved.
  *   Licensed under the MIT License. See License.txt in the project root for license information
  *  --------------------------------------------------------------------------------------------
  */
