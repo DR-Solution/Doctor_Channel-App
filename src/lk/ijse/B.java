e492990673494f85b74c811f2c6410c51ae0c501
DilshanRajika9835 <dilshanrajitha1998@gmail.com>
2021-10-08 21:23:30 +0530

    Create Google Login Use Google Auth close #3

---
+++ src/lk/ijse/B.java
@@ -0,0 +1,21 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *   Copyright (c) 2021. DR Corporation. All rights reserved.
+ *   Licensed under the MIT License. See License.txt in the project root for license information
+ *  --------------------------------------------------------------------------------------------
+ */
+
+package lk.ijse;
+
+/**
+ * @author Dilshan Rajika Withanachchi <dilshanrajitha1998@gmail.com>
+ * @Time :9:16 PM
+ * @Date :2021-10-08/Oct
+ * @Project Name :DocotrChannelApp
+ */
+public class B {
+    public void CreateLogin (){
+        System.out.printf("Hello Google");
+    }
+}
+
---
+++ src/lk/ijse/GoogleLogin.java
@@ -0,0 +1,17 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *   Copyright (c) 2021. DR Corporation. All rights reserved.
+ *   Licensed under the MIT License. See License.txt in the project root for license information
+ *  --------------------------------------------------------------------------------------------
+ */
+
+package lk.ijse;
+
+/**
+ * @author Dilshan Rajika Withanachchi <dilshanrajitha1998@gmail.com>
+ * @Time :10:27 AM
+ * @Date :2021-10-03/Oct
+ * @Project Name :DocotrChannelApp
+ */
+public class GoogleLogin {
+}
